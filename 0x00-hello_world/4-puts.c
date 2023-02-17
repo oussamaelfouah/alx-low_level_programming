c4d90a3165afba1c5dc06c95210f027c01457289
oussama <oelfouah@gmail.com>
2023-02-17 19:04:19 +0100

    betty coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
+ * main - entry point
+ * Return: always 0 (success)
+ */
+int main (void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+alx lox level
